5e4de12310703482b6625e368d0440e3d879e858
Andrey Nazentsev <nazntrade@gmail.com>
2023-09-27 10:07:32 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aston_HW_3_Lists_20"
+include(":app")
+ 
